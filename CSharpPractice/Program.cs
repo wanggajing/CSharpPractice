e061c6b74e92ab4e78532dddf668bc561494fc06
Wu Ke <wke918@aucklanduni.ac.nz>
2015-05-18 12:35:30 +1200

    First commit

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("You have entered {0} command line arguments",args.Length);
+            foreach(string param in args)
+            {
+                Console.WriteLine(param);
+            }
+            Console.ReadLine();
+        }
+    }
+}
